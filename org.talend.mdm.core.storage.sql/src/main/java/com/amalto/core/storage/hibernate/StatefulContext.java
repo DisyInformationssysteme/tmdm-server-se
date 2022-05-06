843925fd0440e11654089c00a910d2f68b69d3fe
Haiting Yin <htyin@talend.com>
2022-05-06 16:51:31 +0800

    TMDM-15244 Error java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2 when deploying new version of Data Model (#1525) (#1531)

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/StatefulContext.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/StatefulContext.java
@@ -92,7 +92,8 @@ public class StatefulContext implements MappingCreatorContext {
                 .filter(item -> !item.getName().equals(field.getDeclaringType().getName())).collect(Collectors.toList());
         outloop: for (ComplexTypeMetadata subType : subTypeSet) {
             for (FieldMetadata fieldMetadata : subType.getFields()) {
-                if (fieldMetadata instanceof ReferenceFieldMetadata && ((ReferenceFieldMetadata) fieldMetadata).isFKIntegrity()
+                if (fieldMetadata instanceof ReferenceFieldMetadata && field instanceof ReferenceFieldMetadata
+                        && ((ReferenceFieldMetadata) fieldMetadata).isFKIntegrity()
                         && fieldMetadata.getName().equals(field.getName())) {
                     isDupFKName = true;
                     break outloop;
@@ -102,7 +103,8 @@ public class StatefulContext implements MappingCreatorContext {
 
         if (type instanceof ContainedComplexTypeMetadata && isDupFKName) {
             ComplexTypeMetadata containedType = ((ContainedComplexTypeMetadata) type).getContainedType();
-            key = String.join("/", containedType.getNamespace(), containedType.getName(), topLevelType.getNamespace(), topLevelType.getName());
+            key = String.join("/", containedType.getNamespace(), containedType.getName(), topLevelType.getNamespace(),
+                    topLevelType.getName(), field.getName());
         }
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("The current qualified key is " + key);
